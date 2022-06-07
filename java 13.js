c3560b897e25bf457408876b605d01b84173e2d2
e-sancam <92087728+e-sancam@users.noreply.github.com>
2022-06-07 10:55:42 +0200

    yes

---
+++ java 13.js
@@ -0,0 +1,60 @@
+// JavaScript Document
+
+let word = "bokstav";
+
+///console.log(ord.lengt);
+
+//console.log(ord[3]);
+
+//console.log(ord.indexOf("tav"));
+
+//console.log(ord.slice[4]);
+
+//console.log(ord.slice(1, -1));
+
+////
+
+let text = "Jeg begynte å fortære en sandwich mens jeg kjørte taxi på vei til en quiz"
+
+let newText = ("Jeg begynte aa fortaere en sandwich mens jeg kjoerte taxi paa vei til en quiz")
+
+///////////////
+
+let cars = ["Ferrari",
+           "Lexus",
+           "Marussia",
+           "Toyota",
+           "Alfa Romeo"];
+
+let cars = {
+
+        brand: "Ferrari",
+        color: "Red",
+        year: 1957
+};
+
+let cars = [
+    
+    {brand: "Ferrari", color: "red", year: 1957},
+    {brand: "Lexus", color: "green", year: 1989},
+    {brand: "Marussia", color: "orange", year: 2009},
+    {brand: "Toyota", color: "black", year: 1990},
+   {brand: "Alfa Romeo", color: "purple", year: 1967}
+    
+];
+
+console.log(cars[3].brand);
+
+
+
+/////////////////////////
+
+let person = {
+
+        name: "Pedro",
+        lastname: "Cola blanca",
+        age: 35
+};
+
+
+
